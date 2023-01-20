478fed217c92e3ea6b195618e156187377269495
vidhya1706 <vidhya.b@myglamm.com>
2023-01-20 23:25:49 +0530

    timer changes

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const newHr = 00;
+const newHr = 0;
 const newMin = 0;
 const newSec = 5;
 
-document.querySelector(".hours").innerHTML = newHr;
-document.querySelector(".minutes").innerHTML = newMin;
-document.querySelector(".seconds").innerHTML = newSec;
+setTime(newHr, newMin, newSec);
 
 let myInterval;
 let now;
 let today;
 let deadline;
+
+const elem = document.getElementsByClassName("action")?.[0];
+
+function setTime(h, m, s) {
+  document.querySelector(".hours").innerHTML =
+    h.toString().length > 1 ? h : `0${h}`;
+  document.querySelector(".minutes").innerHTML =
+    m.toString().length > 1 ? m : `0${m}`;
+  document.querySelector(".seconds").innerHTML =
+    s.toString().length > 1 ? s : `0${s}`;
+}
+
+function onClearInterval() {
+  clearInterval(myInterval);
+  elem.classList.remove("pause");
+  elem.classList.add("play");
+}
 
 function startTimer() {
   const t = deadline - now;
@@ -17,15 +32,15 @@ function startTimer() {
   const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((t % (1000 * 60)) / 1000);
   if (hours === 0 && minutes === 0 && seconds === 0) {
-    clearInterval(myInterval);
+    onClearInterval();
   }
-  document.querySelector(".hours").innerHTML = hours;
-  document.querySelector(".minutes").innerHTML = minutes;
-  document.querySelector(".seconds").innerHTML = seconds;
+  setTime(hours, minutes, seconds);
   now = new Date();
 }
 
 function createTimer() {
+  elem.classList.remove("play");
+  elem.classList.add("pause");
   const today = new Date();
   const currentYear = today.getFullYear();
   const currentMonth = today.getMonth();
@@ -42,5 +57,14 @@ function createTimer() {
     currentSec + newSec
   );
   now = new Date();
+
   myInterval = setInterval(startTimer, 1000);
+}
+
+function onClickAction() {
+  if (elem.classList.contains("play")) {
+    createTimer();
+  } else {
+    onClearInterval();
+  }
 }
